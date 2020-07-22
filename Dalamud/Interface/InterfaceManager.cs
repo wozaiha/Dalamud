3f57608a287855cc8afb36eb4ffdc096f8da05b1
Bluefissure <bluefissure@foxmail.com>
2020-07-22 14:03:39 -0400

    Update dxgi memory offset

--- Dalamud/Interface/InterfaceManager.cs
+++ Dalamud/Interface/InterfaceManager.cs
@@ -68,6 +68,7 @@ namespace Dalamud.Interface
             this.dalamud = dalamud;
 
             try {
+                //throw new Exception("Skip swapchain sig search");
                 var sigResolver = new SwapChainSigResolver();
                 sigResolver.Setup(scanner);
 
